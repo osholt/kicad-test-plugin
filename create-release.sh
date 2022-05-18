4c16812cd0bf00464966e08797f8b9c29c6807ed
Oliver Holt <osholt@me.com>
2022-05-18 23:25:50 +0100

    Automated commit before release

--- create-release.sh
+++ create-release.sh
@@ -21,8 +21,8 @@ git push
 
 if [[ $prerelease -eq 1 ]]
 then
-    gh release create "v${VERSION}" $RELEASE_ARCHIVE_NAME -t "Release ${VERSION}" -n "This was auto-generated." -p 
+    gh release create "v$VERSION" $RELEASE_ARCHIVE_NAME -t "Release $VERSION" -n "This was auto-generated." -p 
 else
-    gh release create "v${VERSION}" $RELEASE_ARCHIVE_NAME -t "Release ${VERSION}" -n "This was auto-generated."
+    gh release create "v$VERSION" $RELEASE_ARCHIVE_NAME -t "Release $VERSION" -n "This was auto-generated."
 fi
 
