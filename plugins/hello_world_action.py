3564a0b8c1d8d010b9ce25e9b7b735e0104c7074
Oliver Holt <osholt@me.com>
2022-05-19 13:44:44 +0100

    Automated commit before release

--- plugins/hello_world_action.py
+++ plugins/hello_world_action.py
@@ -3,9 +3,9 @@ import os
 
 class HelloWorldAction(pcbnew.ActionPlugin):
     def defaults(self):
-        self.name = "A complex action plugin"
-        self.category = "A descriptive category name"
-        self.description = "A description of the plugin"
+        self.name = "Test Plugin"
+        self.category = "Test"
+        self.description = "This plugin opens a window that says Hello World"
         self.show_toolbar_button = True # Optional, defaults to False
         self.icon_file_name = os.path.join(os.path.dirname(__file__), 'hello_world_light.png') # Optional
 
