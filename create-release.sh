3bac9e41abd68599b323cdb7bbd8023e544d9209
Oliver Holt <osholt@me.com>
2022-05-19 12:22:55 +0100

    Automated commit before release

--- create-release.sh
+++ create-release.sh
@@ -38,7 +38,7 @@ cd metadata
 git commit -a -m "Automated update" --quiet
 git push --quiet
 cd ..
-rm -rvf metadata > /dev/null
+#rm -rvf metadata > /dev/null
 
 ##update main repo
 #git clone https://github.com/osholt/kicad-plugin-repo plugin_repo --quiet
